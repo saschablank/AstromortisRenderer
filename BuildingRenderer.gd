1d229509a2d1d7254a7d2cd77e3b33b769790436
Sascha Blank <sascha_blank@hotmail.com>
2025-03-23 22:08:50 +0100

    Building Renderer

--- BuildingRenderer.gd
+++ BuildingRenderer.gd
@@ -1,5 +1,7 @@
 extends Node3D
 
+var is_render_taken: bool = false
+@export var building_name: String = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,6 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if is_render_taken == false and building_name.is_empty() == false:
+		var image: Image = $Camera3D.get_viewport().get_texture().get_image()
+		image.save_png("res://output/buildings/" + building_name + ".png" )
